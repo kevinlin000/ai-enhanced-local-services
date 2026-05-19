4c1296edad5348badfdd26fad6b3e46b3f323cb6
kevinlin000 <guguji1444@gmail.com>
2026-05-19 14:33:26 +0800

    feat：店家地理位置查詢功能模組

--- backend-environment/src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ backend-environment/src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -4,6 +4,7 @@ import cn.hutool.core.util.BooleanUtil;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmdp.dto.Result;
 import com.hmdp.entity.Shop;
@@ -12,13 +13,20 @@ import com.hmdp.service.IShopService;
 import com.hmdp.utils.CacheClient;
 import com.hmdp.utils.RedisConstants;
 import com.hmdp.utils.RedisData;
+import com.hmdp.utils.SystemConstants;
+import org.springframework.data.geo.Distance;
+import org.springframework.data.geo.GeoResult;
+import org.springframework.data.geo.GeoResults;
+import org.springframework.data.redis.connection.RedisGeoCommands;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.domain.geo.GeoReference;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 
 import java.time.LocalDateTime;
+import java.util.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -221,5 +229,56 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
         //2. 刪除緩存
         stringRedisTemplate.delete(CACHE_SHOP_KEY + shop.getId());
         return Result.ok();
+    }
+
+    @Override
+    public Result queryShopByType(Integer typeId, Integer current, Double x, Double y) {
+        // 1. 判斷是否需要根據座標查詢
+        if (x == null || y == null) {
+            // 不需要座標查詢，按照資料庫查
+            Page<Shop> page = query()
+                    .eq("type_id", typeId)
+                    .page(new Page<>(current, SystemConstants.DEFAULT_PAGE_SIZE));
+            // 返回數據
+            return Result.ok(page.getRecords());
+        }
+        // 2. 計算分頁參數
+            int from = (current - 1) * SystemConstants.DEFAULT_PAGE_SIZE;
+            int end = current * SystemConstants.DEFAULT_PAGE_SIZE;
+        // 3. 根據typeId和分頁參數查詢redis，結果按照距離排序
+        String key = SHOP_GEO_KEY + typeId;
+        GeoResults<RedisGeoCommands.GeoLocation<String>> results = stringRedisTemplate.opsForGeo()
+                .search(
+                        key, GeoReference.fromCoordinate(x, y),
+                        new Distance(5000),   // 查詢半徑5公里;
+                        RedisGeoCommands.GeoSearchCommandArgs.newGeoSearchArgs().includeDistance().limit(end));
+        // 4. 解析出id，距離等資訊
+        if (results == null ) {
+            // 沒有查詢到任何店鋪
+            return Result.ok(Collections.emptyList());
+        }
+        List<GeoResult<RedisGeoCommands.GeoLocation<String>>> list = results.getContent();
+        
+        if (list.size() <= from) {
+            return Result.ok(Collections.emptyList());
+        }
+        // 4.1 截取from 到 end 的部分
+        List<Long> ids = new ArrayList<>(list.size());
+        Map<String, Distance> distanceMap = new HashMap<>(list.size());
+        list.stream().skip(from).forEach(result -> {
+                    String shopIdStr = result.getContent().getName();
+                    ids.add(Long.valueOf(shopIdStr));
+                    // 獲取距離
+                    Distance distance = result.getDistance();
+                    distanceMap.put(shopIdStr, distance);
+                });
+        // 5. 根據id從資料庫查詢店家資訊
+        String idStr = StrUtil.join(",", ids);
+        List<Shop> shops = query().in("id", ids).last("ORDER BY FIELD(id," + idStr + ")").list();
+        for (Shop shop : shops) {
+            shop.setDistance(distanceMap.get(shop.getId().toString()).getValue());
+        }
+            // 6. 返回
+            return Result.ok(shops);
     }
 }
--- backend-environment/src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ backend-environment/src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -3,16 +3,25 @@ package com.hmdp;
 import com.hmdp.entity.Shop;
 import com.hmdp.service.impl.ShopServiceImpl;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.geo.Point;
+import org.springframework.data.redis.connection.RedisGeoCommands;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @SpringBootTest
 class HmDianPingApplicationTests {
 
     @Resource
     private ShopServiceImpl shopService;
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
 
     @Test
     void testSaveShop() {
@@ -26,5 +35,34 @@ class HmDianPingApplicationTests {
         }
 
         System.out.println("全部店家快取預熱完成！共預熱了 " + shopList.size() + " 間店鋪。");
+    }
+
+    @Test
+    void loadShopData(){
+        // 1.查詢店家資訊
+        List<Shop> list = shopService.list();
+        // 2. 把店家分組，按雜後typeID分組
+        Map<Long, List<Shop>> map = list.stream().collect(Collectors.groupingBy(Shop::getTypeId));
+        //3.分批完成寫入redis
+        for (Map.Entry<Long, List<Shop>> entry : map.entrySet()) {
+            //3.1 獲取類型ID
+            Long typeId = entry.getKey();
+            String key = "shop:geo:" + typeId;
+           //3.2 獲取同類型的店家的集合
+            List<Shop> value = entry.getValue();
+            List<RedisGeoCommands.GeoLocation<String>> locations = new ArrayList<>(value.size());
+            //3.3 寫入redis GEOADD key  經度 緯度 member
+            for (Shop shop : value) {
+                //stringRedisTemplate.opsForGeo().add(key, new Point(shop.getX(), shop.getY()), shop.getId().toString());
+                locations.add(new RedisGeoCommands.GeoLocation<>(
+                        shop.getId().toString(),
+                        new Point(shop.getX(), shop.getY())
+                ));
+            }
+            stringRedisTemplate.opsForGeo().add(key,locations);
+
+
+
+        }
     }
 }
